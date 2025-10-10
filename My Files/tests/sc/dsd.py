e4ce943e4679b527ad6d45d36abc3d7fc558d4db
Muhammad Sami Ghazi <61657207+mynameissami@users.noreply.github.com>
2025-10-10 19:35:36 +0500

    Delete My Files directory

--- My Files/tests/sc/dsd.py
+++
@@ -1,4 +0,0 @@
-import os
-dir1 = [f for f in os.listdir('.')]
-if "Settings.ini" in dir1:
-     os.remove("Settings.ini")
--- My Files/tests/test33.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-review_url = "https://pastebin.com/raw/kU4sqcN5"
-resp = requests.get(review_url)
-soup = BeautifulSoup(resp.text, 'html.parser')
-print(soup)
-print(soup.find("pre"))
